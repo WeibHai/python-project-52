55b4140f6fed1854671e1283a3040b5c35efc170
Mark (ThinkPad) <proskurina-1956@mail.ru>
2023-06-10 21:24:41 +0300

    final fixs

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,9 +1,10 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
 # Create your models here.
 class Statuses(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name=_('Status name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
