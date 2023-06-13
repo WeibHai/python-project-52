395fdc526be64217c511685e847b0167dc47c31e
Mark (ThinkPad) <proskurina-1956@mail.ru>
2023-06-13 18:47:33 +0300

    fix bugs

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Labels(m.Model):
-    name = m.CharField(max_length=255, nique=True, verbose_name=_('Label name'))
+    name = m.CharField(max_length=255, unique=True, verbose_name=_('Label name'))
     created_at = m.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
 
     def __str__(self):
